a4bb3cfb07077ab095269d871457aee5675d2d99
ppuivif <papuivif@gmail.com>
2026-03-02 13:17:03 +0100

    random value generator added

---
+++ Basic_instructions/05-Random_value_generator/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+int roll1 = random.Next(1, 7);
+int roll2 = random.Next(1, 7);
+int largestValue = Math.Max(roll1, roll2);
+
+Console.WriteLine($"First roll is : {roll1}");
+Console.WriteLine($"First roll is : {roll2}");
+if (roll1 == roll2)
+{
+    Console.WriteLine($"The two values are equal !");
+}
+else
+{
+    Console.WriteLine($"The largest value is : {largestValue}");
+}    
